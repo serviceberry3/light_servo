83e450c70979a8289d954868964bab6282c5346f
Noah Weiner <noah.weiner@yale.edu>
2020-07-27 11:53:32 -0400

    manual servo pulsing

--- LightServo/LightServo.cpp
+++ LightServo/LightServo.cpp
@@ -25,14 +25,22 @@ void loop()
 	//read in the value from pin A1 (value of resistance from photoresistor)
 	int sensor_value = analogRead(A1);
 
-	//check the
-	if (sensor_value > MIN_LIGHT)// the point at which the state of LEDs change
+	//check to see if the resistance value from photoresistor is above threshold
+	if (sensor_value > MIN_LIGHT)
     {
 		//if there's enough light coming in, shake the servo arm back and forth
-		myservo.write(90);
-		delay(200);
-		myservo.write(106);
-		delay(200);
+
+		//set voltage HIGH to start the servo period and pulse
+		digitalWrite(SERVO_PIN, HIGH);
+
+		//delay for length of pulse (1.8 ms)
+		delayMicroseconds(pulseMicroSec);
+
+		//set voltage LOW for remainder of servo pulse
+		digitalWrite(SERVO_PIN, LOW);
+
+		//delay for remainder of servo period so that next signal starts on time
+		delayMicroseconds(periodMicroSec - pulseMicroSec);
     }
 	//ELSE don't move the servo arm
 }
