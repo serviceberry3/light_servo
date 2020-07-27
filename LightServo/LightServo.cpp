02f8b6bedd3030e182ae466887bbecb52e5080a0
Noah Weiner <noah.weiner@yale.edu>
2020-07-27 13:01:22 -0400

    correcting and spacing servo pulses

--- LightServo/LightServo.cpp
+++ LightServo/LightServo.cpp
@@ -17,8 +17,15 @@ void setup()
 //servos are controlled by sending them a 20ms pulse
 int periodMicroSec = 20 * 1000;
 
-//1.0 ms HIGH at the start of pulse will turn the servo to the 0 degree position
-int pulseMicroSec = 1.8 * 1000;
+//0.7 ms HIGH at the start of pulse will turn the servo to the 0 degree position
+//keep the signal HIGH for 0.7 ms, which is equivalent to setting servo to 0 degrees (2.45 ms is equivalent to setting ot 180 degrees)
+int pulseHighMicroSec = 0.7 * 1000;
+
+//keep the signal low for rest of the servo period
+int pulseLowMicroSec;
+
+//on each loop let's up the HIGH duration by 0.01 ms, which is approximately equivalent to moving servo arm by 1 degree on each loop (2450-700)/180
+int angleIncrement = 0.01 * 1000;
 
 void loop()
 {
@@ -30,17 +37,28 @@ void loop()
     {
 		//if there's enough light coming in, shake the servo arm back and forth
 
+		//increase the amt of time the servo signal is kept high by 0.01 ms each time
+		pulseHighMicroSec += angleIncrement;
+
 		//set voltage HIGH to start the servo period and pulse
 		digitalWrite(SERVO_PIN, HIGH);
 
 		//delay for length of pulse (1.8 ms)
-		delayMicroseconds(pulseMicroSec);
+		delayMicroseconds(pulseHighMicroSec);
 
 		//set voltage LOW for remainder of servo pulse
 		digitalWrite(SERVO_PIN, LOW);
 
 		//delay for remainder of servo period so that next signal starts on time
-		delayMicroseconds(periodMicroSec - pulseMicroSec);
+		delayMicroseconds(periodMicroSec - pulseHighMicroSec);
+
+		//this controls speed at which arm moves
+		delay(0.5);
+
+		//if we've reached 180 degrees, start coming back; if reached 0, also reverse
+		if (pulseHighMicroSec >= 2450 || pulseHighMicroSec <=700) {
+			angleIncrement = -angleIncrement;
+		}
     }
 	//ELSE don't move the servo arm
 }
