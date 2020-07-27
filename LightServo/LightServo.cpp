2458034729630bbffef61c8108db018607fd4cb3
Noah Weiner <noah.weiner@yale.edu>
2020-07-27 11:29:28 -0400

    commenting and defining constants

--- LightServo/LightServo.cpp
+++ LightServo/LightServo.cpp
@@ -1,21 +1,38 @@
+//CODE FOR ARDUINO UNO
+
 #include <Arduino.h>
 
+//set the servo motor interaction pin to pin 4
 #define SERVO_PIN 4
+
+//define the threshold value for the photoresistor
+#define MIN_LIGHT 600
 
 void setup()
 {
+	//set the servo motor interaction pin to OUTPUT so we can send pulses to servo
 	pinMode(SERVO_PIN, OUTPUT);
 }
 
+//servos are controlled by sending them a 20ms pulse
+int periodMicroSec = 20 * 1000;
+
+//1.0 ms HIGH at the start of pulse will turn the servo to the 0 degree position
+int pulseMicroSec = 1.8 * 1000;
+
 void loop()
 {
-   int sensor_value = analogRead(A1);
+	//read in the value from pin A1 (value of resistance from photoresistor)
+	int sensor_value = analogRead(A1);
 
-   if (sensor_value > 600)// the point at which the state of LEDs change
+	//check the
+	if (sensor_value > MIN_LIGHT)// the point at which the state of LEDs change
     {
-      myservo.write(90);
-      delay(200);
-      myservo.write(106);
-      delay(200);
+		//if there's enough light coming in, shake the servo arm back and forth
+		myservo.write(90);
+		delay(200);
+		myservo.write(106);
+		delay(200);
     }
+	//ELSE don't move the servo arm
 }
